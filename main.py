a523a493826a5e457d5c36207152e54a61ad0700
savi4m83-crypto <savi4m83@gmail.com>
2026-06-01 20:25:58 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# Write a function celsius_to_fahrenheit(c) that converts temperature from Celsius
+#  to Fahrenheit.
+
+def celcius_to_fahrenheit(c):
+    print((9/5)*c+32)
+c=0
+fahrenheit=((9/5)*c+32)
+celcius=float(input("Enter the celcius: "))
+celcius_to_fahrenheit(celcius)
